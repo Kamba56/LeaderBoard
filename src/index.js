4826c2f0356b29b06c9ef7e2ce58e6f96a635ed1
toma <tomajohn316@yahoo.com>
2022-06-15 17:04:25 +0100

    Add basic setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import { items } from './modules/elements';
+import populate from './modules/populate';
 import './style.css';
+
+items.forEach(populate);
---
+++ src/modules/elements.js
@@ -0,0 +1,25 @@
+const list = document.querySelector('list-container');
+const items = [
+  {
+    name: John,
+    score: 100
+  },
+  {
+    name: John,
+    score: 100
+  },
+  {
+    name: John,
+    score: 100
+  },
+  {
+    name: John,
+    score: 100
+  },
+  {
+    name: John,
+    score: 100
+  },
+];
+
+export {list, items};
